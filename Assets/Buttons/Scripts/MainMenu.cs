a8843aa8ed527b3af461d4995d64bdc0acfb61a2
melta <majorasleo@gmail.com>
2026-02-01 15:12:49 +0100

    Button Start and Quit are working

---
+++ Assets/Buttons/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEditor.Overlays;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+    
+        SceneManager.LoadScene("Mask-LD");
+    }
+    // Permet de lancer le jeu lorsque le bouton start est presser 
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
